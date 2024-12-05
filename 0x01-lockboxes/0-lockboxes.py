9d5410dc706a332c09e62f6c2ae92975cd178a70
Elsmoil <alsmoilalrasta999@gmail.com>
2024-12-05 23:21:22 +0000

    more

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+This module contains the function `canUnlockAll` that determines if all boxes can be unlocked.
+Each box contains keys to other boxes, and the first box is unlocked initially.
+"""
+
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be unlocked.
+
+    Args:
+        boxes (list of list of ints): A list of lists where each sublist represents the keys in a box.
+
+    Returns:
+        bool: True if all boxes can be unlocked, otherwise False.
+    """
+    unlocked = set([0])  # We start by unlocking box 0
+    keys_to_check = [0]  # Start with box 0
+
+    while keys_to_check:
+        current_box = keys_to_check.pop()  # Get the next box to process
+
+        # Loop through the keys in the current box
+        for key in boxes[current_box]:
+            if key not in unlocked:
+            unlocked.add(key)  # Add the key (box) to unlocked set
+            keys_to_check.append(key)  # Add the new unlocked box to the list to check
+
+    # Check if all boxes are unlocked
+    return len(unlocked) == len(boxes)
