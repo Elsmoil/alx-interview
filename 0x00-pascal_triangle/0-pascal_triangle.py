ffe5f721b02568d0ba40692688edabc1d37b6458
Elsmoil <alsmoilalrasta999@gmail.com>
2024-12-05 22:00:51 +0000

    more work

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    
+    triangle = [[1]]  # First row is always [1]
+    
+    for i in range(1, n):
+        row = [1]  # Start the row with [1]
+        
+        # Fill in the middle elements of the row
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        
+        row.append(1)  # End the row with [1]
+        
+        triangle.append(row)  # Add the row to the triangle
+    
+    return triangle
