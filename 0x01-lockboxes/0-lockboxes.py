8ed29a5919587eca97f77e1f80f10be9aa50dda7
Elsmoil <alsmoilalrasta999@gmail.com>
2024-12-06 00:08:41 +0000

    tested

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,24 +13,44 @@ def canUnlockAll(boxes):
     Determines if all the boxes can be unlocked.
 
     Args:
-        boxes (list of list of ints): A list of lists where each
-        sublist represents the keys in a box.
+    boxes (list of list): List of boxes, each containing keys to other boxes.
 
     Returns:
-        bool: True if all boxes can be unlocked, otherwise False.
+    bool: True if all boxes can be opened, False otherwise.
     """
-    unlocked = set([0])  # We start by unlocking box 0
-    keys_to_check = [0]  # Start with box 0
+    # Number of boxes
+    n = len(boxes)
 
+    # A list to track which boxes have been unlocked
+    unlocked = [False] * n
+
+    # The first box is always unlocked
+    unlocked[0] = True
+
+    # A list to store keys we have found and can use to open other boxes
+    keys_to_check = [0]
+
+    # Start exploring the boxes
     while keys_to_check:
-        current_box = keys_to_check.pop()  # Get the next box to process
+        current_box = keys_to_check.pop()
 
-        # Loop through the keys in the current box
+        # Try to unlock boxes using keys found in the current box
         for key in boxes[current_box]:
-            if key not in unlocked:
-                unlocked.add(key)  # Add the key (box) to unlocked set
-            # Add the new unlocked box to the list to check
-            keys_to_check.append(key)
+            if key < n and not unlocked[key]:
+                unlocked[key] = True  # Mark the box as unlocked
+                # Add the box to the list of keys to check
+                keys_to_check.append(key)
 
-    # Check if all boxes are unlocked
-    return len(unlocked) == len(boxes)
+    # If all boxes are unlocked, return True; otherwise, False
+    return all(unlocked)
+
+
+# Test cases
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))  # Expected output: True
+
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))  # Expected output: True
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))  # Expected output: False
